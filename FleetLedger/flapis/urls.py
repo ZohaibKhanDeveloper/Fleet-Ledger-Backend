f09ce31e327f7319088fa6d988740cf3981e52fb
Zohaib khan <zk510873@gmail.com>
2026-03-07 07:15:04 -0800

    Two urls are addes for the driver table

--- FleetLedger/flapis/urls.py
+++ FleetLedger/flapis/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('vehicles/',views.Vehicles.as_view()),
     path('vehicle/<int:id>/',views.VehicleDetail.as_view()),
+    path('drivers/',views.Drivers.as_view()),
+    path('driver/<int:id>/',views.DriverDetail.as_view()),
 ]
