3d01375587cbe0c96bf47e204a21484f611839a3
Zohaib khan <zk510873@gmail.com>
2026-03-20 04:23:44 -0700

    Built-in user views were added for jwt authentication.

--- FleetLedger/flapis/urls.py
+++ FleetLedger/flapis/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenBlacklistView
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path('dashboard/',views.dashboard),
     path('vehicles/',views.Vehicles.as_view()),
     path('vehicle/<int:id>/',views.VehicleDetail.as_view()),
